a133f5944de12e3a523bca56ebf47cb07d0445de
Thiện Lâm <lamminhthien02012000@gmail.com>
2023-11-01 11:06:26 +0700

    add code for anonymous test

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const { Builder, By, Key, until } = require('selenium-webdriver');
 const chrome = require('selenium-webdriver/chrome');
 
+
+const roomCode = "ewp8g"
+
 // Function to create a new WebDriver instance for Chrome
 async function createChromeInstance () {
   let options = new chrome.Options();
@@ -19,33 +22,14 @@ async function createChromeInstance () {
 async function performActions (driver) {
   try {
     // Navigate to a website
-    await driver.get('https://quizne.com');
-
-    // // Find an element and perform an action (e.g., click a link)
-    // const link = await driver.findElement(By.linkText('Example Link'));
-    // await link.click();
-
-    // // Wait for a specific condition (e.g., element to be visible)
-    // await driver.wait(until.elementIsVisible(link), 5000);
-
-    await driver.get("https://www.quizne.com/")
-    // 2 | setWindowSize | 1920x993 | 
-    // await driver.manage().window().setRect({ width: 1920, height: 993 })
-    // // 3 | click | css=.hover\3A cursor-pointer:nth-child(1) > .relative > .rounded-lg | 
-    // await driver.findElement(By.css(".hover\\3A cursor-pointer:nth-child(1) > .relative > .rounded-lg")).click()
-    // // 4 | click | css=.abc-backdrop | 
-    // await driver.findElement(By.css(".abc-backdrop")).click()
-    // // 5 | click | css=.hover\3A cursor-pointer:nth-child(2) > .relative > .rounded-lg | 
-    // await driver.findElement(By.css(".hover\\3A cursor-pointer:nth-child(2) > .relative > .rounded-lg")).click()
-    // // 6 | click | css=.abc-backdrop | 
-    // await driver.findElement(By.css(".abc-backdrop")).click()
-    // // 7 | click | css=.hover\3A cursor-pointer:nth-child(3) > .relative > .rounded-lg | 
-    // await driver.findElement(By.css(".hover\\3A cursor-pointer:nth-child(3) > .relative > .rounded-lg")).click()
-    // // 8 | click | css=.abc-backdrop | 
-    // await driver.findElement(By.css(".abc-backdrop")).click()
-    await driver.pause(5000);
+    await driver.get(`https://www.quizne.com/room/${roomCode}/multiple-play`)
+    await driver.manage().window().setRect({ width: 1920, height: 993 })
+    await driver.findElement(By.name("name")).sendKeys("a")
+    await driver.findElement(By.name("name")).sendKeys(Key.ENTER)
 
   } finally {
+    // Waiting 15 second
+    await driver.sleep(15000);
     // Close the browser
     await driver.quit();
   }
@@ -53,7 +37,7 @@ async function performActions (driver) {
 
 // Create and run multiple instances of Chrome concurrently
 async function main () {
-  const numberOfInstances = 10; // Change this to the number of instances you want to run
+  const numberOfInstances = 2; // Change this to the number of instances you want to run
 
   const drivers = [];
   for (let i = 0; i < numberOfInstances; i++) {
